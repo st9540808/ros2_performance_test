6b391bfb6bafba23bf0ed69053d9a52f4a31fddf
st9540808 <st9540808@gmail.com>
2019-12-12 16:15:43 +0800

    Calcuate difference between unix time and boot time

---
+++ sofa_time.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import ctypes, os
+import time
+
+__all__ = ["get_monotonic_time"]
+
+CLOCK_MONOTONIC_RAW = 4 # see <linux/time.h>
+
+class timespec(ctypes.Structure):
+    _fields_ = [
+        ('tv_sec', ctypes.c_long),
+        ('tv_nsec', ctypes.c_long)
+    ]
+
+librt = ctypes.CDLL('librt.so.1', use_errno=True)
+clock_gettime = librt.clock_gettime
+clock_gettime.argtypes = [ctypes.c_int, ctypes.POINTER(timespec)]
+
+def get_monotonic_time():
+    t = timespec()
+    if clock_gettime(CLOCK_MONOTONIC_RAW , ctypes.pointer(t)) != 0:
+        errno_ = ctypes.get_errno()
+        raise OSError(errno_, os.strerror(errno_))
+    return t.tv_sec + t.tv_nsec * 1e-9
+
+def get_uptime():
+    with open('/proc/uptime', 'r') as f:
+        uptime_seconds = float(f.readline().split()[0])
+    return uptime_seconds
+
+def get_unix_mono_diff():
+    return time.time() - get_monotonic_time()
+
+if __name__ == "__main__":
+    print(get_monotonic_time())
+    print(get_unix_mono_diff())
